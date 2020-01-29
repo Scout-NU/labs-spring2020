e306df63720dc562593d986c2a9d8c734a6f0f18
josephmannis <annis.jo@husky.neu.edu>
2020-01-28 20:54:56 -0500

    Added typography components.

---
+++ labs/src/components/atoms/Typography.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const P = styled.p`
+    font-size: 18px;
+    line-height: 30px;
+`
+
+export const H1 = styled.h1`
+    font-weight: bolder;
+    font-size: 64px;
+`
+
+export const H2 = styled.h2`
+    font-weight: bolder;
+    font-size: 54px;
+`
+
+export const H3 = styled.h3`
+    font-weight: bolder;
+    font-size: 40px;
+`
+
+export const H4 = styled.h4`
+    font-weight: normal;
+    font-size: 36px;
+`
---
+++ labs/src/components/theme/Global.tsx
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    font-family: 'Montserrat', 'Open Sans', 'Helvetica Neue', 'Arial', sans-serif
+`
+
+export default GlobalStyles;
